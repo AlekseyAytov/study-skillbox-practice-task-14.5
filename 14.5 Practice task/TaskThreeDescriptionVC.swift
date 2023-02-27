b4bdd87d934fe8996584ca72ea1ae030555149da
AlekseyAytov <aytov88@gmail.com>
2023-02-27 22:42:44 +0300

    Add TaskThree view controller

---
+++ 14.5 Practice task/TaskThreeDescriptionVC.swift
@@ -0,0 +1,29 @@
+//
+//  TaskThreeDescriptionVC.swift
+//  14.5 Practice task
+//
+//  Created by Alex Aytov on 2/27/23.
+//
+
+import UIKit
+
+class TaskThreeDescriptionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
